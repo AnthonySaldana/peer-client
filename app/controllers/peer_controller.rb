5cd4d89cfa0564aa3d8265884013077dde6560cc
Anthony Saldana <anthonywebsol@gmail.com>
2019-01-02 15:15:51 -0800

    fix url

--- app/controllers/peer_controller.rb
+++ app/controllers/peer_controller.rb
@@ -4,7 +4,7 @@ class PeerController < ApplicationController
   def index
     zip = params[:zip].to_i
     if zip != 0
-      url = 'https://tranquil-mountain-63685.herokuapp.com/peer?zip=' + zip.to_s
+      url = 'https://tranquil-citadel-34866.herokuapp.com/peer?zip=' + zip.to_s
       response = HTTParty.get(url)
       return render json: response
     end
